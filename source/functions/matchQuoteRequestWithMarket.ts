95ebb638735664d6dc41d6cafbb0df13fbd8a6a0
Naim Buru <nbur4u@gmail.com>
2021-02-15 04:42:10 +0300

    add matchQuoteRequestWithMarket function & tests

---
+++ source/functions/matchQuoteRequestWithMarket.ts
@@ -0,0 +1,67 @@
+import convertFtxOrderDataToJson from "./convertFtxOrderDataToJson"
+
+interface FtxOrder {
+    [index: number]: number
+}
+
+interface QuoteRequestBody {
+    action: string,
+    base_currency: string,
+    quote_currency: string,
+    amount: string,
+}
+
+export default (quote: QuoteRequestBody, asks: FtxOrder[], bids: FtxOrder[]) => {
+    const getSum = (array: number[]) => {
+        return array.reduce(function (sum, value) {
+            return sum + value;
+        }, 0);
+    }
+
+    if (quote.action === "buy") {
+        // asks are sellers
+        let remainingAmount = parseFloat(quote.amount)
+        let prices = []
+        const jsonAsks = asks.map(ask => convertFtxOrderDataToJson(ask))
+        for (let jsonAsk of jsonAsks) {
+
+            if (jsonAsk.amount < remainingAmount) {
+                remainingAmount -= jsonAsk.amount;
+                prices.push(jsonAsk.price * jsonAsk.amount)
+            } else if (jsonAsk.amount >= remainingAmount) {
+                prices.push(jsonAsk.price * remainingAmount)
+                break;
+            }
+        }
+        if (remainingAmount > 0) {
+            return { price: getSum(prices) / (parseFloat(quote.amount) - remainingAmount), }
+
+        } else {
+            return { price: getSum(prices) / parseFloat(quote.amount), }
+        }
+
+    } else {
+        // bids are buyers
+        let remainingAmount = parseFloat(quote.amount)
+        let prices = []
+        const jsonBids = bids.map(bid => convertFtxOrderDataToJson(bid))
+        for (let jsonBid of jsonBids) {
+
+            if (jsonBid.amount < remainingAmount) {
+                remainingAmount -= jsonBid.amount;
+                prices.push(jsonBid.price * jsonBid.amount)
+            } else if (jsonBid.amount >= remainingAmount) {
+                prices.push(jsonBid.price * remainingAmount)
+                break;
+            }
+        }
+        if (remainingAmount > 0) {
+            return { price: getSum(prices) / (parseFloat(quote.amount) - remainingAmount), }
+
+        } else {
+            return { price: getSum(prices) / parseFloat(quote.amount), }
+        }
+
+    }
+
+}
---
+++ test/matchQuoteRequestWithMarket.spec.ts
@@ -0,0 +1,35 @@
+import matchQuoteRequestWithMarket from "../source/functions/matchQuoteRequestWithMarket";
+import orderBookResultExample from "./ftxOrderbookResultExample"
+import { expect } from 'chai';
+
+describe("matchQuoteRequestWithMarket function tests", function () {
+    it("matches quote with best offers", async () => {
+        // asks: [46898.0, 1],...
+        const { price } = matchQuoteRequestWithMarket({
+            "action": "buy",
+            "base_currency": "BTC",
+            "quote_currency": "USD",
+            "amount": "1"
+        }, orderBookResultExample.asks, orderBookResultExample.bids);
+        expect(price).to.equal(46898.0)
+
+        // bids: [46894.0, 3.7684],...
+        const { price: price2 } = matchQuoteRequestWithMarket({
+            "action": "sell",
+            "base_currency": "BTC",
+            "quote_currency": "USD",
+            "amount": "3.7684"
+        }, orderBookResultExample.asks, orderBookResultExample.bids);
+        expect(price2).to.equal(46894.0)
+
+        // bids: [46894.0, 3.7684], [46893.0, 0.064],...
+        const { price: price3 } = matchQuoteRequestWithMarket({
+            "action": "sell",
+            "base_currency": "BTC",
+            "quote_currency": "USD",
+            "amount": "3.8324"
+        }, orderBookResultExample.asks, orderBookResultExample.bids);
+        expect(price3).to.be.approximately(46893.98330028181, 0.00001);
+    });
+
+});
